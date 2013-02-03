a54a89f1b691676d95755d934f4633ec48231c9b
Jeremy Kuzub <jeremykuzub@gmail.com>
2013-02-03 15:00:52 -0500

    added legit sample content, tuned transitions

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
 	ff.max=-1;//number of questions
 	ff.correct = 0;//you suck!
 	ff.attempted = 0;//try harder!
-	ff.htmlNext='<div class="btnnext">Next!</div>';
+	ff.htmlNext='<a href="#" class="btnnext">Next!</a>';
 	
 	ff.handleBtnClick = function(e){
 		$(e.target).parent().parent().find('.answer').css('visibility','visible');
@@ -29,9 +29,6 @@
 		$('.btnnext:eq('+ff.currIndex+')').css('visibility','visible');
 		var y=$(window).scrollTop() + $(window).height();
 		var myy = $('.btnnext:eq('+ff.currIndex+')').offset().top;
-		//myy+=$('.btnnext:eq('+ff.currIndex+')').height();
-		console.log('lowest visible row of window:'+y);
-		console.log('start row of next btn:'+myy);
 		if(myy+40>y){
 			//hidden below; scroll down to top of photo:
 			console.log('scrolling');
@@ -39,9 +36,6 @@
 				scrollTop: $('.btnnext:eq('+(ff.currIndex)+')').offset().top - $(window).height() + $('.btnnext:eq('+(ff.currIndex)+')').height()
 			}, 200);
 		}
-		//$('.btnnext:eq('+ff.currIndex+')').css('position','relative');
-		//$('.btnnext:eq('+ff.currIndex+')').css('top',y-40+'px');
-		
 	}
 	
 	ff.handleBtnStart = function(e){
@@ -78,9 +72,9 @@
 			var pct = Math.floor(100.0*ff.correct/ff.attempted);
 			var comment='';
 			em='Thanks for playing!<br/>Your Score:<br/><span style="font-size:3em;font-weight:bold;">'+pct+'%</span><br/>';
-			if(pct <= 10) comment = 'Wow, that was abysmal. We have got some serious Wax-On/Wax-Off to do.';
-			if(pct>10) comment = 'These questions are designed such that a score worse than chance (50%) indicates you have some misconceptions about nuclear. But that is why we are here. As Yoda said, "...you must unlearn what you have learned..."';
-			if(pct>=50) comment = 'Not too shabby, you are either lucky, or know a bit about nuclear. Still, I wouldn not put you in charge of a nuclear plant or anything';
+			if(pct <= 10) comment = 'Wow, that was abysmal. We have got some serious Wax-On/Wax-Off to do. Or you were just butt-dialing this whole quiz.';
+			if(pct>10) comment = 'Worse than guessing (50%)? That indicates you have some misconceptions about nuclear. But that is why we are here. As Yoda said, "...you must unlearn what you have learned..."';
+			if(pct>=50) comment = 'Not too shabby, you are either lucky, or know a bit about nuclear. Still, I would not put you in charge of a nuclear plant or anything, nothing personal.';
 			if(pct>75) comment = 'Wow, you know a thing or two about nuclear. Our algorithms are impressed. Have you thought a bout a career in being Mostly Right about Stuff?';
 			if(pct>99) comment = 'You are a living, breathing, genius. Or you clicked View Source.';
 			em+=comment;
@@ -91,8 +85,7 @@
 				scrollTop: $('.finalscore').offset().top
 			}, 400);
 		}
-			
-		
+		return false; // avoid jump to '#'
 	}
 	
 	ff.init = function(){
@@ -100,16 +93,8 @@
 			scrollTop: 0
 		}, 400);
 		$('.factorfiction').hide(); 
-		//$('.choices:eq('+ff.currIndex+')').css('display','inherit');
 		$('.fof').css('display','none');
-		//$('.fof:eq('+ff.currIndex+')').css('display','inherit');
 		$('.answer').css('visibility','hidden');
-		
-		//$('a.btnfact').css('background-color','#dd0');
-		$('a').click(function(evt){
-			//... do stuff
-			return false; // avoid jump to '#'
-		})
 		$('a.btnfact').bind('click', $.proxy(ff.handleBtnClick, this));
 		$('a.btnfiction').bind('click', $.proxy(ff.handleBtnClick, this));
 		
@@ -119,14 +104,18 @@
 		//hide finish screen
 		$('.finalscore').css('display','none');
 
-		//$(ff.htmlNext).appendTo('.fof');
 		$('.fof').append(ff.htmlNext);
 		$('.btnnext').bind('click', $.proxy(ff.handleBtnNext, this));
 		$('.btnnext').css('cursor','pointer');
 		$('.btnnext').css('visibility','hidden');
+
+		$('a').click(function(evt){
+			return false; // avoid jump to '#'
+		})
 		
 		//start button handler
 		$('.btnstart').bind('click', $.proxy(ff.handleBtnStart, this));
+
 		//show everything now:
 		$('.factorfiction').fadeIn(400);
 		
